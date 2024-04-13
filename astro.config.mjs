b65e72f05fdc1686828a69ffea5fee626655a27c
Shashank Bhat <shashank.bhat2525@gmail.com>
2024-04-14 02:08:39 +0530

    [fix] robot.txt -1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.fireshort.in",
   integrations: [tailwind({
-    applyBaseStyles: false,
-  }), react()]
+    applyBaseStyles: false
+  }), react(), robotsTxt()]
 });
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export function calculateRotation(index) {
+  return index % 2 === 0 ? 5 * index : -5 * index;
+}
+
